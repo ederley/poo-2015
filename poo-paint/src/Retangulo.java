d3cf74a01e077a5f410b014f8fa0b49d8032d1a8
ederley <edersilva2080@gmail.com>
2015-03-02 22:41:27 -0300

    pratica 2

---
+++ poo-paint/src/Retangulo.java
@@ -0,0 +1,25 @@
+
+public class Retangulo {
+	private int x;
+	private int y;
+	private int altura;
+	private int largura;
+	
+	public Retangulo(int x,int y,int altura,int largura){
+		
+		this.x = x;
+		this.y = y;
+		this.altura = altura;
+		this.largura = largura;
+		String s = "hello";
+		String s2 = s + "ederley";
+		
+	}
+	
+	public String imprime(){
+		return "Retangulo [x:" + this.x + " y:" + this.y + " largura:" + this.largura + " altura:" + this.altura + "]";
+	}
+	
+  }
+
+
