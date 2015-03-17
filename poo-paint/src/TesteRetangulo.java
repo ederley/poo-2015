f792513ee49353116ca6c68d7056d41f16828c99
313123890 <313123890@BARLAB808.una.edu.br>
2015-03-16 21:53:30 -0300

    atividades de get's

--- poo-paint/src/TesteRetangulo.java
+++ poo-paint/src/TesteRetangulo.java
@@ -1,6 +1,9 @@
 
+
 import org.junit.Assert;
 import org.junit.Test;
+
+import figuras.Retangulo;
 
 
 public class TesteRetangulo {
@@ -8,6 +11,10 @@ public class TesteRetangulo {
 	public void testaRetangulo(){
 		
 		Retangulo retangulo1 = new Retangulo(23, 14, 30, 20);
+		
+		Assert.assertEquals(14, retangulo1.getY());
+		
+		
 		String saida = retangulo1.imprime();
 		Assert.assertEquals("Retangulo [x:23 y:14 largura:20 altura:30]", saida);
 		
--- poo-paint/src/figuras/Retangulo.java
+++ poo-paint/src/figuras/Retangulo.java
@@ -1,3 +1,4 @@
+package figuras;
 
 public class Retangulo {
 	private int x;
@@ -19,6 +20,26 @@ public class Retangulo {
 	public String imprime(){
 		return "Retangulo [x:" + this.x + " y:" + this.y + " largura:" + this.largura + " altura:" + this.altura + "]";
 	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public int getLargura() {
+		return largura;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}	
 	
   }
 
